f11859e05059b83b4858f30faa95d0fbf1d45f2b
pursharth25 <18ucc059@lnmiit.ac.in>
2023-03-23 18:57:30 +0530

    added event handler

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import { industriesData } from "./data/industriesData";
+import { countriesData } from "./data/countriesData";
 
+
+function useOutsideAlerter(ref, action) {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        action(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, action]);
+}
 
 function App() {
   return (
