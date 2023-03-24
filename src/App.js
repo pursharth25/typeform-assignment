412a67e18427aa584e23dc929f0858f5f7d3d253
pursharth25 <18ucc059@lnmiit.ac.in>
2023-03-24 19:01:47 +0530

    added required states

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import Button from "./components/Button";
+import ErrorMsg from "./components/Error";
 
 function useOutsideAlerter(ref, action) {
   useEffect(() => {
@@ -16,6 +17,39 @@ function useOutsideAlerter(ref, action) {
 }
 
 function App() {
+
+  const [submitScreen, setSubmitScreen] = useState(false);
+  const [widthArray, setWidthArray] = useState([0, 0, 0, 0, 0, 0, 0]);
+  const [selectBlock, setSelectedBlock] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+  const [roleActive, setRoleActive] = useState("");
+  const [goals, setGoals] = useState([]);
+  const [searchCountryModal, setSearchCountryModal] = useState(false);
+  const [searchCountryValue, setSearchCountryValue] = useState("");
+
+  const [country, setCountry] = useState({
+    name: "India",
+    code: "IN",
+    phone: 91,
+  });
+
+  const [formValues, setFormValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+
+  const [errorMsg, setErrorMsg] = useState({
+    firstName: false,
+    lastName: false,
+    email: false,
+    phone: false,
+    industry: false,
+    role: false,
+    goals: false,
+  });
+
   return (
     <div className="App">
      <h1>hello</h1>
